773f9015ea3192a2d7d2bab51bca91a80281d09d
guilhermegcaires <guilhermegcaires@gmail.com>
2022-09-28 20:28:09 -0300

    GroupCard component created

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
+import { GroupCard } from '@components/GroupCard';
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Turma da Medicina"]);
+
   return (
     <Container>
-
+      <Header/>
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+        />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          
+          />
+        )}
+        />
     </Container>
   );
 }
