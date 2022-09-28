f3d15499770733e56c32a8f2941f7d8bba20048c
guilhermegcaires <guilhermegcaires@gmail.com>
2022-09-27 21:20:01 -0300

    Initialized project

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from "react-native";
 import { Groups } from '@screens/Groups';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from '@components/Loading';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from './src/theme';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  
+
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        />
+      {fontsLoaded ?  <Groups/> : <Loading/>}
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
